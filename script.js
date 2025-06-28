bdfafa5fe57d83454b87e472d63f42ac0ebbea19
ladiesdrop <sathikhanom172@gmail.com>
2025-06-28 15:29:06 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// টপ বাটন ফাংশনালিটি
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// কারেন্ট ইয়ার ফুটারে আপডেট করবে
+document.addEventListener('DOMContentLoaded', function() {
+    const yearSpan = document.getElementById('current-year');
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+});
